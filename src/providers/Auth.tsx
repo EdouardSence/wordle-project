fb76f895e4094a9fd332d93e0595af5e50620b9a
esence <edouard.sence@u-bordeaux.fr>
2025-11-18 10:27:26 +0100

    chore: suppression du fichier google-services.json et des dépendances Firebase dans app.json

---
+++ src/providers/Auth.tsx
@@ -0,0 +1,15 @@
+// AuthProviderType = {
+//   user: User | null;
+//     initializing: boolean;
+// };
+
+// const AuthContext = React.createContext<AuthProviderType | null>(null);
+
+// export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+//   const [user, setUser] = useState<User | null>(null);
+//     const [initializing, setInitializing] = useState(true);
+// }
+
+// const { mutateAsync : loginUser} = useMutation({
+//     mutationFn: apiDataSource.login,
+// });
